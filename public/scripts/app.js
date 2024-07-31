7bb0759c02f7d80e06f4c8a0ff1681b58bbb1a0c
yiming <58835791+yiming0@users.noreply.github.com>
2024-07-31 11:29:51 +0000

    wip

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,7 @@
-export const registerServiceWorker = async (worker) => {
+export const registerServiceWorker = async (worker_script) => {
     if ('serviceWorker' in navigator) {
         try {
-            const registration = await navigator.serviceWorker.register(worker, { scope: './', })
-            if (registration.installing) {
-                console.log('Service worker installing')
-            } else if (registration.waiting) {
-                console.log('Service worker installed')
-            } else if (registration.active) {
-                console.log('Service worker active')
-            }
+            const worker = await navigator.serviceWorker.register(worker_script, { scope: './', })
         } catch (error) {
             console.error(`Registration failed with ${error}`)
         }
--- public/worker.js
+++ public/worker.js
@@ -1,82 +1,39 @@
-const addResourcesToCache = async (resources) => {
-    const cache = await caches.open('v1')
-    await cache.addAll(resources)
-  }
+const fetchStorage = async () => await caches.open('fetch')
+
+const cacheResources = async () => {
+  const storage = await fetchStorage()
+
+  storage.addAll([
+    './',
+    './index.html',
+    './scripts/app.js',
+    './icons/512.png',
+  ])
+}
+
+const cacheableFetch = async ({ request }) => {
+
+  const cachedResponse = await caches.match(request)
+  if (cachedResponse) return cachedResponse
+
+  const response = await fetch(request.clone())
   
-  const putInCache = async (request, response) => {
-    const cache = await caches.open('v1')
-    await cache.put(request, response)
-  }
-  
-  const cacheFirst = async ({ request, preloadResponsePromise, fallbackUrl }) => {
-    // First try to get the resource from the cache
-    const responseFromCache = await caches.match(request)
-    if (responseFromCache) {
-      return responseFromCache
-    }
-  
-    // Next try to use the preloaded response, if it's there
-    // NOTE: Chrome throws errors regarding preloadResponse, see:
-    // https://bugs.chromium.org/p/chromium/issues/detail?id=1420515
-    // https://github.com/mdn/dom-examples/issues/145
-    // To avoid those errors, remove or comment out this block of preloadResponse
-    // code along with enableNavigationPreload() and the "activate" listener.
-    const preloadResponse = await preloadResponsePromise
-    if (preloadResponse) {
-      console.info('using preload response', preloadResponse)
-      putInCache(request, preloadResponse.clone())
-      return preloadResponse
-    }
-  
-    // Next try to get the resource from the network
-    try {
-      const responseFromNetwork = await fetch(request.clone())
-      // response may be used only once
-      // we need to save clone to put one copy in cache
-      // and serve second one
-      putInCache(request, responseFromNetwork.clone())
-      return responseFromNetwork
-    } catch (error) {
-      const fallbackResponse = await caches.match(fallbackUrl)
-      if (fallbackResponse) {
-        return fallbackResponse
-      }
-      // when even the fallback response is not available,
-      // there is nothing we can do, but we must always
-      // return a Response object
-      return new Response('Network error happened', {
-        status: 408,
-        headers: { 'Content-Type': 'text/plain' },
-      })
-    }
-  }
-  
-  const enableNavigationPreload = async () => {
-    if (self.registration.navigationPreload) {
-      // Enable navigation preloads!
-      await self.registration.navigationPreload.enable()
-    }
-  }
-  
-  self.addEventListener('activate', (event) => {
-    event.waitUntil(enableNavigationPreload())
-  })
-  
-  self.addEventListener('install', (event) => {
-    event.waitUntil(
-      addResourcesToCache([
-        './',
-        './index.html',
-        './scripts/app.js',
-      ])
-    )
-  })
-  
-  self.addEventListener('fetch', (event) => {
-    event.respondWith(
-      cacheFirst({
-        request: event.request,
-        preloadResponsePromise: event.preloadResponse,
-      })
-    )
-  })
+  const storage = await fetchStorage()
+  storage.put(request, response)
+
+  return response
+}
+
+self.addEventListener('activate', (event) => {
+  console.log('[worker] active')
+})
+
+self.addEventListener('install', (event) => {
+  console.log('[worker] install')
+
+  event.waitUntil(cacheResources())
+})
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(cacheableFetch(event))
+})
